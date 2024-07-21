908bd1aba0e5cfc6a264bac8fbed0c668a6419b7
child-play-neurips <child.play.neurips@gmail.com>
2024-07-21 21:20:44 +0200

    server is up and running

--- chem-aca-q-a/scripts/scrapers.py
+++ chem-aca-q-a/scripts/scrapers.py
@@ -3,86 +3,94 @@ import re
 from typing import Tuple, Optional
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
+from requests.exceptions import ConnectionError, HTTPError
+import time
 
 from .pdf_utils import download_pdf
 
-def scrape_page_articles_springer(url: str, output_folder: str, csv_path: str, journal_name: str, vpn_index: int) -> Tuple[Optional[str], int, int]:
-    """Scrape articles from a webpage and download PDFs.
-    
-    Args:
-        url (str): The URL of the webpage.
-        count (int): The current count of downloaded PDFs.
-        vpn_index (int): The index of the current VPN location.
-        vpn_locations (List[str]): List of VPN locations.
+def scrape_page_articles_springer(url: str, output_folder: str, csv_path:str, journal_name: str, vpn_index: int) -> Tuple[int, int]:
+    """Scrape articles from Springer's website and download PDFs of articles using concurrent futures for parallelization."""
 
-    Returns:
-        Tuple[Optional[str], int, int]: The URL of the next page, updated count, and updated VPN index.
-    """
+    current_url = url
     count = 0
 
-    page = requests.get(url + "/articles")
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.content, 'html.parser')
-        pdf_links = soup.find_all('a', attrs={'data-test': 'pdf-link'})
-        base_url = url
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            for link in pdf_links:
-                print("="*50)
-                future = executor.submit(download_pdf, base_url + link['href'], output_folder, csv_path, journal_name, base_url + link['href'])
-                count += 1
-                
-        next_page_link = soup.find('a', attrs={'data-test': 'next-page'})
-        if next_page_link:
-            return base_url + next_page_link['href'], count, vpn_index
-        else:
-            return None, count, vpn_index
-    else:
-        print(f"Failed to retrieve the webpage. Status code: {page.status_code}")
-        return None, count, vpn_index
+    while current_url:
+        response = requests.get(current_url)
+        if response.status_code != 200:
+            print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+            break
 
-def scrape_page_articles_rsc(url: str, output_folder: str, csv_path: str, journal_name: str, vpn_index: int) -> Tuple[Optional[str], int, int]:
-    """
-    Scrape articles from an RSC journal webpage, download PDFs, and navigate to the next page.
-
-    Args:
-        url (str): The URL of the webpage.
-        journal_name (str): The name of the journal for logging purposes.
-        count (int): The current count of downloaded PDFs.
-        vpn_index (int): The index of the current VPN location.
-        vpn_locations (List[str]): List of VPN locations.
-
-    Returns:
-        Tuple[Optional[str], int, int]: The URL of the next page, updated count, and updated VPN index.
-    """
-    count = 0
-
-    page = requests.get(url)
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.content, 'html.parser')
-        pdf_links = soup.find_all('a', class_='btn btn--primary btn--tiny', href=True)
-        next_page_btn = soup.find('a', class_='paging__btn paging__btn--next', aria_disabled="false")
+        soup = BeautifulSoup(response.content, 'html.parser')
+        article_items = soup.select('.content-card')
 
         with ThreadPoolExecutor(max_workers=5) as executor:
             futures = []
-            for link in pdf_links:
-                pdf_url = url + link['href']
-                futures.append(executor.submit(download_pdf, url, output_folder, csv_path, journal_name, pdf_url))
+            for article_item in article_items:
+                article_page_link = article_item.find('a')['href']
+                article_response = requests.get(article_page_link)
+                article_soup = BeautifulSoup(article_response.content, 'html.parser')
+                pdf_link_element = article_soup.find('span', text='Download PDF').find_parent('a')
+
+                if pdf_link_element:
+                    pdf_url = pdf_link_element['href']
+                    print(f"Found PDF link: {pdf_url}")
+                    futures.append(executor.submit(download_pdf, pdf_url, output_folder, csv_path, journal_name, pdf_url))
 
             for future in futures:
                 if future.result():
                     count += 1
+                    print(f"Downloaded {count} PDFs")
 
-        if next_page_btn and next_page_btn.has_attr('data-pageno'):
-            next_page_no = next_page_btn['data-pageno']
-            next_page_url = f"{url.split('#')[0]}#!recentarticles&adv&page={next_page_no}"
-            return next_page_url, count, vpn_index
+        # Pagination
+        next_page_link = soup.find('a', rel='next', class_='next_page')
+        current_url = next_page_link['href'] if next_page_link and 'href' in next_page_link.attrs else None
+
+    print(f"Finished downloading. Total PDFs downloaded: {count}")
+    return None, count, vpn_index
+
+def scrape_page_articles_rsc(url: str, output_folder: str, csv_path:str, journal_name: str, vpn_index: int) -> Tuple[Optional[str], int, int]:
+    """Scrape articles from RSC's website and download PDFs of articles using concurrent futures for parallelization."""
+
+    current_url = url
+    count = 0
+
+    while current_url:
+        response = requests.get(current_url)
+        print(response)
+        exit()
+        if response.status_code != 200:
+            print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+            break
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        article_items = soup.find_all('div', class_='capsule capsule--article')
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            futures = []
+            for article_item in article_items:
+                pdf_link_element = article_item.find('a', class_='btn btn--primary btn--tiny')
+                if pdf_link_element:
+                    pdf_url = pdf_link_element['href']
+                    print(f"Found PDF link: {pdf_url}")
+                    futures.append(executor.submit(download_pdf, pdf_url, output_folder, csv_path, journal_name, pdf_url))
+
+            for future in futures:
+                if future.result():
+                    count += 1
+                    print(f"Downloaded {count} PDFs")
+
+        # Pagination
+        next_page_link = soup.find('a', class_='paging__btn paging__btn--next', attrs={'aria-disabled': 'false'})
+        if next_page_link and 'data-pageno' in next_page_link.attrs:
+            next_page_no = next_page_link['data-pageno']
+            current_url = f"{url.split('?')[0]}?page={next_page_no}"
         else:
-            return None, count, vpn_index
-    else:
-        print(f"Failed to retrieve the webpage. Status code: {page.status_code}")
-        return None, count, vpn_index
+            break
+
+    print(f"Finished downloading. Total PDFs downloaded: {count}")
+    return None, count, vpn_index
     
-def scrape_page_articles_acs(url: str, output_folder: str, csv_path:str, journal_name: str, base_url: str, vpn_index: int) -> Tuple[int, int]:
+def scrape_page_articles_acs(url: str, output_folder: str, csv_path:str, journal_name: str, vpn_index: int) -> Tuple[int, int]:
     """
     Scrape open access articles from an ACS, navigate to PDF page, and download PDFs.
 
@@ -91,7 +99,6 @@ def scrape_page_articles_acs(url: str, output_folder: str, csv_path:str, journal
         journal_name (str): The name of the journal for logging purposes.
         vpn_index (int): The index of the current VPN location.
         vpn_locations (List[str]): List of VPN locations.
-        base_url (str): The base URL of the journal for constructing full links.
 
     Returns:
         Tuple[int, int]: Updated count of downloaded PDFs and updated VPN index.
@@ -110,13 +117,13 @@ def scrape_page_articles_acs(url: str, output_folder: str, csv_path:str, journal
                 if open_access_img:
                     pdf_link_element = article.find('a', title="PDF")
                     if pdf_link_element:
-                        pdf_page_url = base_url + pdf_link_element['href']
+                        pdf_page_url = url + pdf_link_element['href']
                         pdf_page = requests.get(pdf_page_url)
                         if pdf_page.status_code == 200:
                             pdf_soup = BeautifulSoup(pdf_page.content, 'html.parser')
                             download_button = pdf_soup.find('a', class_='navbar-download')
                             if download_button and 'href' in download_button.attrs:
-                                final_pdf_url = base_url + download_button['href']
+                                final_pdf_url = url + download_button['href']
                                 futures.append(executor.submit(download_pdf, final_pdf_url, output_folder, csv_path, journal_name, final_pdf_url))
 
             for future in futures:
@@ -129,7 +136,7 @@ def scrape_page_articles_acs(url: str, output_folder: str, csv_path:str, journal
 
     return count, vpn_index
     
-def scrape_page_articles_nature(url: str, output_folder: str, csv_path: str, journal_name: str, vpn_index: int) -> Tuple[Optional[str], int, int]:
+def scrape_page_articles_nature(url: str, output_folder: str, csv_path:str, journal_name: str, base_url: str, vpn_index: int) -> Tuple[int, int]:
     """Scrape articles from Nature's website and download PDFs of open-access articles using concurrent futures for parallelization."""
 
     current_url = url
@@ -151,6 +158,7 @@ def scrape_page_articles_nature(url: str, output_folder: str, csv_path: str, jou
                     pdf_link_element = article_item.find('a', {'data-article-pdf': 'true', 'data-test': 'download-pdf'})
                     if pdf_link_element:
                         pdf_url = url + pdf_link_element['href']
+                        print(f"Found PDF link: {pdf_url}")
                         futures.append(executor.submit(download_pdf, pdf_url, output_folder, csv_path, journal_name, current_url))
 
             for future in futures:
